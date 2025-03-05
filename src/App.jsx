5c190f8c3f2c132dc6171a0f686e31b7a3592d1b
Kishore1245 <shreekishorekishore.s@gmail.com>
2025-03-05 20:28:43 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Portfolio from "./Components/profile/Portfolio";
 import Navbar from "./Components/layout/Navbar";
-import Contact from "./Components/sources/Contact";
-import AboutMe from "./Components/sources/Aboutme";
-import Skills from "./Components/sources/Skills";
+import Contact from "./Components/Source/Contact";
+import AboutMe from "./Components/Source/Aboutme";
+import Skills from "./Components/Source/Skills";
 import Footer from "./Components/layout/Footer";
-import Projects from "./Components/sources/Projects";
-import Resume from "./Components/sources/Resume";
+import Projects from "./Components/Source/Projects";
+import Resume from "./Components/Source/Resume";
 
 
 function App() {
